3f9e37fc210279779c45ab70a045de28f8617cd5
Priyank Nasit <170010107024@adit.ac.in>
2019-11-14 22:39:35 +0530

    Add files via upload

---
+++ table.php
@@ -0,0 +1,50 @@
+<html>
+<head>
+ <title>Table with database</title>
+ <style>
+  table {
+   border-collapse: collapse;
+   width: 100%;
+   color: #588c7e;
+   font-family: monospace;
+   font-size: 25px;
+   text-align: left;
+     } 
+  th {
+   background-color: #588c7e;
+   color: white;
+    }
+  tr:nth-child(even) {background-color: #f2f2f2}
+ </style>
+</head>
+<body>
+ <table>
+ <tr>
+  <th>firstname</th> 
+  <th>lastname</th> 
+  <th>id</th>
+ </tr>
+
+<?php
+$conn = mysqli_connect("localhost", "root", "", "databaseName");
+  // Check connection
+  if ($conn->connect_error) {
+   die("Connection failed: " . $conn->connect_error);
+  } 
+  $sql = "SELECT id, firstname,lastname FROM tableName";
+  $result = $conn->query($sql);
+  if ($result->num_rows > 0) {
+   // output data of each row
+   while($row = $result->fetch_assoc()) {
+    echo "<tr><td>" . $row["id"]. "</td><td>" . $row["firstname"] . "</td><td>"
+. $row["lastname"]. "</td></tr>";
+}
+echo "</table>";
+} else { echo "0 results"; }
+$conn->close();
+?>
+ 
+
+</table>
+</body>
+</html>
